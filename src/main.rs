ebb9f72a4c7dd62d6a9dfbf3444b334ebce8ea47
shhr1214 <souhei0079@gmail.com>
2018-10-02 01:44:23 +0900

    add: actix-web
    ハローワールドができるようにした。
    ここから機能を増やしていく。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "Hello World"
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
 }
